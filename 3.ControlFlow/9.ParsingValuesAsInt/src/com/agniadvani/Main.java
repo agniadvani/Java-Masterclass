22cbb6da8c9179fe1db31f312eeaf82204af036a
agniadvani <advani.agni.agni@gmail.com>
2020-07-06 01:29:46 +0530

    parse int

---
+++ 3.ControlFlow/9.ParsingValuesAsInt/src/com/agniadvani/Main.java
@@ -0,0 +1,13 @@
+package com.agniadvani;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String numberAsString = "250";
+        int parsedInt = Integer.parseInt(numberAsString);
+        System.out.println(parsedInt);
+        String doubleAsString = "234.56";
+        double parsedDouble = Double.parseDouble(doubleAsString);
+        System.out.println(parsedDouble);
+    }
+}
