632a699521436c63d622ec6a55e423a4b41b9801
agniadvani <advani.agni.agni@gmail.com>
2020-07-05 17:18:41 +0530

    control flow

---
+++ 3.ControlFlow/1.Switch/src/com/agniadvani/Main.java
@@ -0,0 +1,28 @@
+package com.agniadvani;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char value = 'f';
+        switch (value) {
+            case 'A':
+                System.out.println("value is A");
+                break;
+            case 'B':
+                System.out.println("Value is B");
+                break;
+            case 'C':
+                System.out.println("Value is C");
+                break;
+            case 'D':
+                System.out.println("Value is D");
+                break;
+            case 'E':
+                System.out.println("Value is E");
+                break;
+            default:
+                System.out.println("value not found");
+                break;
+        }
+    }
+}
